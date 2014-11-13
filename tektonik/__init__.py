31c8f5afdb80675f0ae03d26240a7a15de1f48ef
Kevin Lint <kevin@flowroute.com>
2014-11-13 09:50:30 -0800

    update init

--- tektonik/__init__.py
+++ tektonik/__init__.py
@@ -16,7 +16,7 @@ def create_app(object_name, env="prod"):
 
     Arguments:
         object_name: the python path of the config object,
-                     e.g. ark.settings.ProdConfig
+                     e.g. tektonik.settings.ProdConfig
 
         env: The name of the current environment, e.g. prod or dev
     """
@@ -41,6 +41,6 @@ if __name__ == '__main__':
     # Import the config for the proper environment using the
     # shell var APPNAME_ENV
     env = os.environ.get('APPNAME_ENV', 'prod')
-    app = create_app('ark.settings.%sConfig' % env.capitalize(), env=env)
+    app = create_app('tektonik.settings.%sConfig' % env.capitalize(), env=env)
 
     app.run()
