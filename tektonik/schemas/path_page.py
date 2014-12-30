49fe12bf80e9c4802e21c306e654ec2a80faa52f
Kevin Lint <kevin@flowroute.com>
2014-12-30 09:32:11 -0800

    tweak

--- tektonik/schemas/path_page.py
+++ tektonik/schemas/path_page.py
@@ -2,6 +2,7 @@ from marshmallow import fields
 from marshmallow import Schema
 from marshmallow import ValidationError
 from tektonik.models.path_page import PathPage as PathPageModel
+from tektonik.models.page import Page as PageModel
 
 
 class PathPage(Schema):
@@ -21,12 +22,13 @@ def validate_path_page_exists(schema, input_data):
 
     path_id = input_data.get('path_id', None)
     page_id = input_data.get('page_id', None)
+    page = PageModel.query.get(page_id)
 
     if path_id is None:
         raise ValidationError('Path is required', 'path_id')
 
-    if page_id is None:
-        raise ValidationError('Page is required', 'page_id')
+    if not page:
+        raise ValidationError('Page not found', 'page_id')
 
 
 @PathPage.validator
